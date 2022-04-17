a29c5fe2092a2291bfec52a70af0d6dc9fed23fa
JLL32 <eabduljalil@outlook.com>
2022-04-17 03:15:55 +0000

    add an abstract auth component

---
+++ components/authForm.tsx
@@ -0,0 +1,24 @@
+import { Box, Flex, Input, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useSWRConfig } from "swr";
+
+const AuthForm = ({ mode }: { mode: "signin" | "signup" }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  return (
+    <Box height="100vh" width="100vw" bg="black" color="gray">
+      <Flex justify="center" align="center" height="100px">
+        hello
+      </Flex>
+      <Flex justify="center" align="center" height="calc(100vh - 100px)">
+        form
+      </Flex>
+    </Box>
+  );
+};
+
+export default AuthForm;
