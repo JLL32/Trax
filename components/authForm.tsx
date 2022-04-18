4381d5189a9eda29d3df87731470648282ca6869
Abdeljalil Elhachimi <eabduljalil@outlook.com>
2022-04-18 16:55:11 +0000

    add form to authForm

--- components/authForm.tsx
+++ components/authForm.tsx
@@ -1,21 +1,62 @@
+import NextImage from "next/image";
 import { Box, Flex, Input, Button } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, FC, FormEventHandler } from "react";
 import { useSWRConfig } from "swr";
+import { auth } from "../lib/mutations";
 
-const AuthForm = ({ mode }: { mode: "signin" | "signup" }) => {
+const AuthForm: FC<{ mode: "signin" | "signup" }> = ({ mode }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
   const router = useRouter();
+  const handleSubmit: FormEventHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    await auth(mode, { email, password });
+    setLoading(false);
+	router.push("/");
+  };
 
   return (
-    <Box height="100vh" width="100vw" bg="black" color="gray">
-      <Flex justify="center" align="center" height="100px">
-        hello
+    <Box height="100vh" width="100vw" bg="black" color="gray.300">
+      <Flex
+        justify="center"
+        align="center"
+        height="100px"
+        borderBottom="while 1px solid"
+      >
+        <NextImage src="/logo.svg" height={60} width={120} />
       </Flex>
       <Flex justify="center" align="center" height="calc(100vh - 100px)">
-        form
+        <Box padding="50px" bg="gray.900" borderRadius="6px">
+          <form onSubmit={handleSubmit}>
+            <Input
+              required
+              placeholder="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              required
+              placeholder="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="submit"
+              bg="green.500"
+              isLoading={isLoading}
+              sx={{
+                "&:hover": {
+                  bg: "green.400",
+                },
+              }}
+            >
+              {mode}
+            </Button>
+          </form>
+        </Box>
       </Flex>
     </Box>
   );
