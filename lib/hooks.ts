9d21936bd740ccbd9046388e58d6330b05566712
JLL32 <eabduljalil@outlook.com>
2022-04-19 23:30:53 +0000

    fix some types

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -16,7 +16,7 @@ export const usePlaylist = () => {
   const { data, error } = useSWR("/playlist", fetcher);
 
   return {
-    playlists: (data as Playlist) || [],
+    playlists: (data as Playlist[]) || [],
     isLoading: !data && !error,
     isError: error,
   };
--- pages/api/playlist.ts
+++ pages/api/playlist.ts
@@ -1,10 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { User } from "@prisma/client";
 import prisma from "../../lib/prisma";
 import { validateRoute } from "../../lib/auth";
 
-export default validateRoute(async (req, res, user) => {
-  const playlists = await prisma.playlist.findMany({
-    where: { userId: user.id },
-    orderBy: { name: "asc" },
-  });
-  res.json(playlists);
-});
+export default validateRoute(
+  async (_req: NextApiRequest, res: NextApiResponse, user: User) => {
+    const playlists = await prisma.playlist.findMany({
+      where: { userId: user.id },
+      orderBy: { name: "asc" },
+    });
+    res.json(playlists);
+  }
+);
