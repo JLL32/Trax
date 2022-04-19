4c408bf365dae68480096ca8c1fd9d1a67c0ce32
JLL32 <eabduljalil@outlook.com>
2022-04-19 23:20:13 +0000

    fix cookie name

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,8 +5,7 @@ import prisma from "./prisma";
 
 export const validateRoute = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
-    const token = req.cookies.TRAX_ACCESS_TOKEN;
-
+    const token = req.cookies["TRAX-ACCESS-TOKEN"];
     if (token) {
       let user: User;
       try {
--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,7 +4,7 @@ const signedinPages = ["/", "/playlist", "/library"];
 
 export default function middleware(req: NextRequest) {
   if (signedinPages.includes(req.nextUrl.pathname)) {
-    const token = req.cookies.TRAX_ACCESS_TOKEN;
+    const token = req.cookies["TRAX-ACCESS-TOKEN"];
 
     if (!token) {
       return NextResponse.redirect("/signin");
