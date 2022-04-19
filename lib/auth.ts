b91df6ba95253e00b3eabd0bca9e0601a7a0c735
JLL32 <eabduljalil@outlook.com>
2022-04-19 18:05:01 +0000

    add a wrapper function to generate protected api handlers

---
+++ lib/auth.ts
@@ -0,0 +1,27 @@
+import { User } from "@prisma/client";
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./prisma";
+
+export const validateRoute = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.TRAX_ACCESS_TOKEN;
+
+    if (token) {
+      let user: User;
+      try {
+        const { id } = jwt.verify(token, "hello") as any;
+        user = await prisma.user.findUnique({ where: { id } });
+        if (!user) throw Error("Not real user");
+      } catch (e) {
+        res.status(401);
+        res.json({ error: "Not Authorized" });
+        return;
+      }
+
+      return handler(req, res, user);
+    }
+    res.status(401);
+    res.json({ error: "Not Authorized" });
+  };
+};
