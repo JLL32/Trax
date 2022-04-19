d63304432446d814b2de934c346e58ae570d9de2
JLL32 <eabduljalil@outlook.com>
2022-04-19 23:22:55 +0000

    replaces static playlist data with online data

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,6 @@
 import NextImage from "next/image";
 import NextLink from "next/link";
+import { Playlist } from "@prisma/client";
 import {
   Box,
   List,
@@ -17,6 +18,7 @@ import {
   MdPlaylistAdd,
   MdFavorite,
 } from "react-icons/md";
+import { usePlaylist } from "../lib/hooks";
 
 const navMenu = [
   {
@@ -49,11 +51,9 @@ const musicMenu = [
   },
 ];
 
-const playlists = Array(30)
-  .fill(10)
-  .map((_, i) => `Playlist ${i}`);
-
 const Sidebar = () => {
+  const {playlists} = usePlaylist();
+
   return (
     <Box
       width="100%"
@@ -101,11 +101,11 @@ const Sidebar = () => {
         <Divider bg="gray.900" />
         <Box height="66%" overflowY="auto" paddingY="20px">
           <List spacing={2}>
-            {playlists.map((playlist) => (
-              <ListItem paddingX="20px" key={playlist}>
+            {playlists.map((playlist: Playlist) => (
+              <ListItem paddingX="20px" key={playlist.id}>
                 <LinkBox>
                   <NextLink href="/" passHref>
-                    <LinkOverlay>{playlist}</LinkOverlay>
+                    <LinkOverlay>{playlist.name}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
