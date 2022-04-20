2d9dc3eab5ac92276e19b62d9b865d27cff1fa9d
JLL32 <eabduljalil@outlook.com>
2022-04-20 03:43:05 +0000

    add playlists count  to user data

--- pages/api/me.ts
+++ pages/api/me.ts
@@ -1,9 +1,13 @@
 import { User } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { validateRoute } from "../../lib/auth";
+import prisma from "../../lib/prisma";
 
 export default validateRoute(
-  (req: NextApiRequest, res: NextApiResponse, user: User) => {
-    res.json(user);
+  async (req: NextApiRequest, res: NextApiResponse, user: User) => {
+    const playlistsCount = await prisma.playlist.count({
+      where: { userId: user.id },
+    });
+    res.json({ ...user, playlistsCount });
   }
 );
